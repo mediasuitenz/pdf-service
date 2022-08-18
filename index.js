488cebb55cb33db07a41f198d4c3563def326c77
Steve Foster <steve.foster@mediasuite.co.nz>
2022-08-18 23:17:59 +1200

    SUPMWS-273 Add accesslog and a log handler

--- index.js
+++ index.js
@@ -4,9 +4,50 @@ const mw = require("./lib/middleware");
 const actions = require("./lib/actions");
 const app = require("express")();
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const winston = require("winston");
 const argv = require("minimist")(process.argv.slice(2));
 const bodyLimit = argv.limit || "50mb";
 const port = argv.port || 3000;
+
+process.on("SIGINT", () => {
+  console.info("Interrupted");
+  process.exit(0);
+});
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: "info",
+      json: true,
+    }),
+  ],
+});
+
+const morganJSONFormat = () =>
+  JSON.stringify({
+    method: ":method",
+    url: ":url",
+    http_version: ":http-version",
+    remote_addr: ":remote-addr",
+    remote_addr_forwarded: ":req[x-forwarded-for]", //Get a specific header
+    response_time: ":response-time",
+    status: ":status",
+    content_length: ":res[content-length]",
+    timestamp: ":date[iso]",
+    user_agent: ":user-agent",
+  });
+
+app.use(
+  morgan(morganJSONFormat(), {
+    stream: {
+      write: (message) => {
+        const data = JSON.parse(message);
+        return logger.info("accesslog", data);
+      },
+    },
+  })
+);
 
 app.get("/ping", actions.ping);
 
